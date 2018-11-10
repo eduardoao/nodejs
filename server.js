d3185e7dc5234b4bdc3d0c219a0931b0a959f53d
Eduardo Alcantara de Oliveira~ <eoalcantara@gmail.com>
2018-11-10 17:52:35 -0200

    Fail jquery foreach

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
+var app     = express();
+
+var messages = [
+    { name: 'Ronda', message: 'Hi' },
+    { name: 'Jamil', message: 'Whats up?'}
+    
+];
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extends: false}));
+//Here we call a route.
+app.get('/messages', (req, res)=> {
+    res.send();
+});
+// ------//
+//Post new message
+app.post('/messages', (req, res)=> {
+    console.log('Body:' + req.body);
+    messages.push(req.body);   
+    res.sendStatus(200);
+});
+
 var server = app.listen(3000, () => {
     console.log('Server is runnig ...', Date() +  server.address().port);
 });
